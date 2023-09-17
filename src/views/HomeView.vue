ecdd390d5f327c51f8650a9573bfa035e0981c8d
RoderCode <Rodersnow@gmail.com>
2023-09-17 08:34:32 +0200

    add html content for price card

--- src/views/HomeView.vue
+++ src/views/HomeView.vue
@@ -1,6 +1,33 @@
 <template>
   <!-- HOME -->
-  <div></div>
+  <div class="px-4 py-12">
+    <!-- PRICE CARD -->
+    <div class="border-black border-1">
+      <header>
+        <h1>Free</h1>
+      </header>
+      <main>
+        <!-- PRICE TAG -->
+        <span>0</span>
+        <span>Greate for startes</span>
+        <span>Discover how to create your first projects</span>
+        <BaseButtonLink name="Get started" />
+        <!-- FEATURE LIST -->
+        <ul>
+          <!-- FEATURE -->
+          <li>
+            <span>New projects / mount</span>
+          </li>
+          <li>
+            <span>Interaction</span>
+          </li>
+          <li>
+            <span>Assets library</span>
+          </li>
+        </ul>
+      </main>
+    </div>
+  </div>
 </template>
 
 <script lang="ts">
@@ -8,11 +35,13 @@ import { defineComponent } from "vue";
 
 import baselineSignalWifi1Bar from "~icons/ic/baseline-signal-wifi-1-bar";
 import BaseButton from "../components/base/BaseButton.vue";
+import BaseButtonLink from "../components/base/BaseButtonLink.vue";
 
 export default defineComponent({
   components: {
     baselineSignalWifi1Bar,
     BaseButton,
+    BaseButtonLink,
   },
   name: "HomeView",
   data() {
